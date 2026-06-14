f510ec7e849cd0276a771118efd9477fa1d8e673
Sharad Pokharel <shrdpokharel@gmail.com>
2026-06-14 10:37:33 +0545

    main_window setup done

--- frontend/ui/windows/main_window.py
+++ frontend/ui/windows/main_window.py
@@ -1,16 +1,24 @@
 from PySide6.QtWidgets import QWidget, QHBoxLayout
-from ui.widgets.topbar import Topbar
-from ui.widgets.footbar import Footbarbar
-from ui.widgets.toolbar import Toolbar
-from ui.widgets.topbar import Topbar
+
+from ui.widgets.sidebar import Sidebar
+from ui.widgets.content import Contentbar
 class Mainwindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Desktop Certificate Manager")
+        self.layout()
+        self.create_widgets()
+        self.add_widgets()
         
         
     def layout(self):
-        main_layout=QHBoxLayout(self)
+        self.main_layout=QHBoxLayout(self)
         
     def create_widgets(self):
-        pass
+        self.sidebar=Sidebar()
+        self.content=Contentbar()
+        
+        
+    def add_widgets(self):
+        self.main_layout.addWidget(self.sidebar, stretch=15)
+        self.main_layout.addWidget(self.content, stretch=85)
